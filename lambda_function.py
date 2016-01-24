9b57b808c59436787fcc24573e2f67a547278d86
samkeen <sam.sjk@gmail.com>
2016-01-23 21:22:05 -0800

    cleanup/re-org; getting ready to add user login

--- lambda_function.py
+++ lambda_function.py
@@ -1,35 +1,60 @@
 from __future__ import print_function
 
 import boto3
-import json
-
-print('Loading function')
 
 
 def lambda_handler(event, context):
-    """Provide an event that contains the following keys:
-
-      - method: one of the operations in the operations dict below
-      - payload: a parameter to pass to the operation being performed
     """
-    # print("Received event: " + json.dumps(event, indent=2))
+    Simple front controller for the few resource methods of this example
 
+    :param event:
+    :param context:
+    :return:
+    """
+    resource = event.get('resource')
+    operation = event.get('operation')
+    if not resource:
+        raise ValueError("'resource' parameter required")
+    if not operation:
+        raise ValueError("'operation' parameter required")
+    resource = resource.lower()
+    operation = operation.lower()
+
+    resource_routes = {
+        'debug': lambda event, context, operation: debug(event, context, operation),
+        'users': lambda event, context, operation: users(event, context, operation),
+        'cafe': lambda event, context, operation: cafes(event, context, operation),
+    }
+
+    if resource not in resource_routes:
+        raise ValueError("Unknown resource: '{}'".format(resource))
+
+    return resource_routes[resource](event, context, operation)
+
+
+def debug(event, context, operation):
     debug_operations = {
         'echo': lambda x: x,
         'ping': lambda x: 'pong'
     }
+    if operation not in debug_operations:
+        raise ValueError('Unrecognized operation: {}'.format(operation))
+
+    return debug_operations[operation](event.get('payload'))
+
+
+def users(event, context, operation):
+    pass
+
+
+def cafes(event, context, operation):
     dynamo_operations = {
-        'PUT': lambda x: dynamo.put_item(**x),
-        'GET': lambda x: dynamo.get_item(**x),
-        'DELETE': lambda x: dynamo.delete_item(**x),
-        'GET_COLLECTION': lambda x: dynamo.scan(**x)
+        'put': lambda x: dynamo.put_item(**x),
+        'get': lambda x: dynamo.get_item(**x),
+        'delete': lambda x: dynamo.delete_item(**x),
+        'get_collection': lambda x: dynamo.scan(**x)
     }
 
-    operation = event.get('operation')
-    if not operation:
-        raise ValueError('"operation" parameter required')
-    if operation in debug_operations:
-        return debug_operations[operation](event.get('payload'))
     if operation not in dynamo_operations:
         raise ValueError('Unrecognized operation: {}'.format(operation))
 
@@ -37,11 +62,11 @@ def lambda_handler(event, context):
     dynamo = boto3.resource('dynamodb').Table(table_name)
 
     payload = {}
-    if operation in ('GET', 'DELETE'):
+    if operation in ('get', 'delete'):
         payload = {
             "Key": event.get('payload')
         }
-    elif operation == 'PUT':
+    elif operation == 'put':
         payload = {
             "Item": event.get('payload')
         }
